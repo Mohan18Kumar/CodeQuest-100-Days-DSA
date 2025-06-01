fbabc80a04763232ce51e33395ddd24608628af1
ABUZAR <abuzaryaseen118@gmail.com>
2025-06-01 16:40:29 +0530

    day40_dp_fibonacci

---
+++ submissions/Abuzar_Yaseen/day40_dp__fibonacci.py
@@ -0,0 +1,10 @@
+def fibonacci(n,mem):
+    mem = {}
+    if n in mem:
+        return mem[n]
+    if n <= 1:
+        return n
+    mem[n] = fibonacci(n-1,mem) + fibonacci(n-2,mem)
+    return mem[n]
+n = int(input("Enter n: "))
+print(fibonacci(n))
